67e60264bd113d0f6187f2a1629af0659f548600
Kim-suk <sukyung01212@gmail.com>
2025-04-01 17:09:26 +0900

    real

---
+++ workspace/animal/src/main/java/com/test/animal/dog_board/dao/DogBoardDAO.java
@@ -0,0 +1,23 @@
+package com.test.animal.dog_board.dao;
+ 
+ import java.util.List;
+ import java.util.Map;
+ 
+ import com.test.animal.dog_board.dto.DogArticleDTO;
+ import com.test.animal.dog_board.dto.DogImageDTO;
+ 
+ public interface DogBoardDAO {
+ 
+ 	List<DogArticleDTO> dog_listArticles();
+ 	int dog_selectNewArticleNo();
+ 	int dog_insertNewArticle(Map<String, Object> articleMap);
+ 	DogArticleDTO dog_viewArticle(int articleNo);
+ 	void dog_updateArticle(Map<String, Object> articleMap);
+ 	void dog_deleteArticle(int articleNo);
+ 	int dog_selectNewImageFileNo();
+ 	void dog_insertNewImage(List<DogImageDTO> imageFileList);
+ 	List<DogImageDTO> dog_selectImageFileList(int articleNo);
+ 	int dog_deleteImage(int imageFileNo);
+ 	int dog_selectArticleNo(int imageFileNo);
+ 
+ }
---
+++ workspace/animal/src/main/java/com/test/animal/dog_board/dao/DogBoardDAOImpl.java
@@ -0,0 +1,90 @@
+package com.test.animal.dog_board.dao;
+ 
+ import java.util.List;
+ import java.util.Map;
+ 
+ import org.apache.ibatis.session.SqlSession;
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.stereotype.Repository;
+ 
+ import com.test.animal.dog_board.dto.DogArticleDTO;
+ import com.test.animal.dog_board.dto.DogImageDTO;
+ 
+ @Repository
+ public class DogBoardDAOImpl implements DogBoardDAO {
+ 	@Autowired
+ 	private SqlSession sqlSession;
+ 	
+ 	@Override
+ 	public List<DogArticleDTO> dog_listArticles() {
+ 		// TODO Auto-generated method stub
+ 		return sqlSession.selectList("mapper.board.dog_selectAllArticlesList");
+ 	}
+ 
+ 	@Override
+ 	public int dog_selectNewArticleNo() {
+ 		// TODO Auto-generated method stub
+ 		return sqlSession.selectOne("mapper.board.dog_selectNewArticleNo");
+ 	}
+ 
+ 	@Override
+ 	public int dog_insertNewArticle(Map<String, Object> articleMap) {
+ 		// TODO Auto-generated method stub
+ 		sqlSession.insert("mapper.board.dog_insertNewArticle", articleMap);
+ 		return (Integer) articleMap.get("articleNo");
+ 	}
+ 
+ 	@Override
+ 	public DogArticleDTO dog_viewArticle(int articleNo) {
+ 		// TODO Auto-generated method stub
+ 		return sqlSession.selectOne("mapper.board.dog_selectArticle", articleNo);
+ 	}
+ 
+ 	@Override
+ 	public void dog_updateArticle(Map<String, Object> articleMap) {
+ 		// TODO Auto-generated method stub
+ 		sqlSession.update("mapper.board.dog_updateArticle", articleMap);
+ 	}
+ 
+ 	@Override
+ 	public void dog_deleteArticle(int articleNo) {
+ 		// TODO Auto-generated method stub
+ 		sqlSession.delete("mapper.board.dog_deleteArticle", articleNo);
+ 	}
+ 
+ 	@Override
+ 	public int dog_selectNewImageFileNo() {
+ 		// TODO Auto-generated method stub
+ 		return sqlSession.selectOne("mapper.board.dog_selectNewImageFileNo");
+ 	}
+ 
+ 	@Override
+ 	public void dog_insertNewImage(List<DogImageDTO> imageFileList) {
+ 		// TODO Auto-generated method stub
+ 		sqlSession.insert("mapper.board.dog_insertNewImage", imageFileList);
+ 	}
+ 
+ 	@Override
+ 	public List<DogImageDTO> dog_selectImageFileList(int articleNo) {
+ 		// TODO Auto-generated method stub
+ 		return sqlSession.selectList("mapper.board.dog_selectImageFileList", articleNo);
+ 	}
+ 
+ 	@Override
+ 	public int dog_deleteImage(int imageFileNo) {
+ 		// TODO Auto-generated method stub
+ 		return sqlSession.delete("mapper.board.dog_deleteImage", imageFileNo);
+ 	}
+ 
+ 	@Override
+ 	public int dog_selectArticleNo(int imageFileNo) {
+ 		// TODO Auto-generated method stub
+ 		return sqlSession.selectOne("mapper.board.dog_selectArticleNo", imageFileNo);
+ 	}
+ 
+ }
+ 
+ 
+ 
+ 
+ 
